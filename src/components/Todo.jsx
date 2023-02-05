c935313cdc1d489d4fc1f0b5bba3856f7a3f88c8
Alex <claptongabriel@gmail.com>
2023-02-05 17:28:58 +0300

    adding filter options

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,70 +6,59 @@ import { useSelector } from "react-redux";
 import theme from "../theme";
 import { TodoItem } from "./TodoItem";
 import { TodoFooter } from "./TodoFooter";
+import { filters } from "../store/todoSlice";
 
 function Todo() {
-  const [filteredTodos, setFilteredTodos] = useState([]);
-  const todos = useSelector((state) => state.todos.todos);
+	const [filteredTodos, setFilteredTodos] = useState([]);
+	const todos = useSelector((state) => state.todos.todos);
+	const filter = useSelector((state) => state.todos.filterBy);
 
-  // const allTodos = () => {
-  //   setFilteredTodos(todos);
-  // };
+	const filteredTodo = () => {
+		if (filter === filters.COMPLETED) {
+			return todos.filter((todo) => todo.isComplted);
+		}
+		if (filter === filters.NOT_COMPLETED) {
+			return todos.filter((todo) => !todo.isCompleted);
+		}
+		return todos;
+	};
 
-  // const activeTodos = () => {
-  //   setFilteredTodos(todos.filter((todo) => todo.isCompleted === false));
-  // };
+	useEffect(() => {
+		setFilteredTodos(() => filteredTodo());
+		console.log(filteredTodos);
+	}, [todos, filter]);
 
-  // const completedTodos = () => {
-  //   setFilteredTodos(todos.filter((todo) => todo.isCompleted === true));
-  // };
-
-  // const clearCompleted = () => {
-  //   setTodos(todos.filter((todo) => todo.isCompleted === false));
-  // };
-
-  // useEffect(() => {
-  //   setFilteredTodos(todos);
-  // }, [todos]);
-
-  return (
-    <Box
-      bg={useColorModeValue(
-        theme.colors.light.veryLightGray,
-        theme.colors.dark.veryDarkDesaturatedBlue
-      )}
-      borderRadius={10}
-    >
-      <Reorder.Group
-        axis="y"
-        values={todos}
-        onReorder={todos}
-        // onReorder={setTodos}
-        style={{ listStyle: "none", overflow: "hidden" }}
-        layoutScroll
-      >
-        {todos?.map((todo) => {
-          return (
-            <Reorder.Item key={todo.id} value={todo}>
-              <TodoItem
-                key={todo.id}
-                item={todo}
-                id={todo.id}
-                text={todo.text}
-                isCompleted={todo.isCompleted}
-              />
-            </Reorder.Item>
-          );
-        })}
-      </Reorder.Group>
-      <TodoFooter
-        todos={todos}
-        // allTodos={allTodos}
-        // activeTodos={activeTodos}
-        // completedTodos={completedTodos}
-        // clearCompleted={clearCompleted}
-      />
-    </Box>
-  );
+	return (
+		<Box
+			bg={useColorModeValue(
+				theme.colors.light.veryLightGray,
+				theme.colors.dark.veryDarkDesaturatedBlue
+			)}
+			borderRadius={10}
+		>
+			<Reorder.Group
+				axis="y"
+				values={filteredTodos}
+				onReorder={setFilteredTodos}
+				style={{ listStyle: "none", overflow: "hidden" }}
+				layoutScroll
+			>
+				{filteredTodos?.map((todo) => {
+					return (
+						<Reorder.Item key={todo.id} value={todo}>
+							<TodoItem
+								key={todo.id}
+								id={todo.id}
+								text={todo.text}
+								isCompleted={todo.isCompleted}
+							/>
+						</Reorder.Item>
+					);
+				})}
+			</Reorder.Group>
+			<TodoFooter todos={todos} />
+		</Box>
+	);
 }
 
 export { Todo };
--- src/components/TodoFooter.jsx
+++ src/components/TodoFooter.jsx
@@ -1,72 +1,79 @@
 import { Box, Text, Flex, Grid, Button } from "@chakra-ui/react";
 import { useState } from "react";
+import { clearCompleted } from "../store/todoSlice";
+import { useDispatch } from "react-redux";
+import { filterBy, filters } from "../store/todoSlice";
 
 export function TodoFooter(props) {
-  const { todos, allTodos, activeTodos, completedTodos, clearCompleted } =
-    props;
+	const { todos } = props;
 
-  const [activeIndex, setActiveIndex] = useState(0);
+	const [activeIndex, setActiveIndex] = useState(0);
+	const dispatch = useDispatch();
 
-  return (
-    <Box py={3} px={5}>
-      <Grid
-        gridTemplateColumns={{
-          base: "2fr 1fr",
-          sm: "3fr 1fr",
-          md: "1fr 3fr 1fr",
-        }}
-        gridTemplateRows={{
-          base: "auto",
-          md: "auto",
-        }}
-        gridTemplateAreas={{
-          base: `'num clear' 'buttons buttons'`,
-          md: `'num buttons clear'`,
-        }}
-      >
-        <Text gridArea="num" fontSize={["12px", "14px", "16px"]} m="auto 0">
-          {todos.length} items left
-        </Text>
-        <Flex gridArea="buttons" justifyContent="center">
-          <Button
-            fontSize={["12px", "14px", "16px"]}
-            isActive={activeIndex === 0 ? true : false}
-            onClick={() => {
-              allTodos();
-              setActiveIndex(0);
-            }}
-          >
-            All
-          </Button>
-          <Button
-            fontSize={["12px", "14px", "16px"]}
-            isActive={activeIndex === 1 ? true : false}
-            onClick={() => {
-              activeTodos();
-              setActiveIndex(1);
-            }}
-          >
-            Active
-          </Button>
-          <Button
-            fontSize={["12px", "14px", "16px"]}
-            isActive={activeIndex === 2 ? true : false}
-            onClick={() => {
-              completedTodos();
-              setActiveIndex(2);
-            }}
-          >
-            Completed
-          </Button>
-        </Flex>
-        <Button
-          gridArea="clear"
-          fontSize={["12px", "14px", "16px"]}
-          onClick={clearCompleted}
-        >
-          Clear Completed
-        </Button>
-      </Grid>
-    </Box>
-  );
+	return (
+		<Box py={3} px={5}>
+			<Grid
+				gridTemplateColumns={{
+					base: "2fr 1fr",
+					sm: "3fr 1fr",
+					md: "1fr 3fr 1fr",
+				}}
+				gridTemplateRows={{
+					base: "auto",
+					md: "auto",
+				}}
+				gridTemplateAreas={{
+					base: `'num clear' 'buttons buttons'`,
+					md: `'num buttons clear'`,
+				}}
+			>
+				<Text
+					gridArea="num"
+					fontSize={["12px", "14px", "16px"]}
+					m="auto 0"
+				>
+					{todos.length} items left
+				</Text>
+				<Flex gridArea="buttons" justifyContent="center">
+					<Button
+						fontSize={["12px", "14px", "16px"]}
+						isActive={activeIndex === 0 ? true : false}
+						onClick={() => {
+							dispatch(filterBy(filters.ALL));
+							setActiveIndex(0);
+						}}
+					>
+						All
+					</Button>
+					<Button
+						fontSize={["12px", "14px", "16px"]}
+						isActive={activeIndex === 1 ? true : false}
+						onClick={() => {
+							dispatch(filterBy(filters.NOT_COMPLETED));
+							setActiveIndex(1);
+						}}
+					>
+						Active
+					</Button>
+					<Button
+						fontSize={["12px", "14px", "16px"]}
+						isActive={activeIndex === 2 ? true : false}
+						onClick={() => {
+							dispatch(filterBy(filters.COMPLETED));
+							setActiveIndex(2);
+						}}
+					>
+						Completed
+					</Button>
+				</Flex>
+				<Button
+					gridArea="clear"
+					fontSize={["12px", "14px", "16px"]}
+					onClick={() => dispatch(clearCompleted(false))}
+				>
+					Clear Completed
+				</Button>
+			</Grid>
+		</Box>
+	);
 }
