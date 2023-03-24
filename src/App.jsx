a8a3f2b8a412ca93b3a880fa62c6359334eb4a11
Alex <claptongabriel@gmail.com>
2023-03-24 09:11:30 +0300

    some light changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Container, Box, Image, useColorModeValue } from "@chakra-ui/react";
-import { useState } from "react";
 
 import { Header } from "./components/Header";
 import { TodoInput } from "./components/TodoInput";
@@ -7,28 +6,31 @@ import { Todo } from "./components/Todo";
 import theme from "./theme";
 
 function App() {
-  return (
-    <>
-      <Box>
-        <Image
-          w="100%"
-          h="15rem"
-          fit="cover"
-          src={useColorModeValue(theme.bg.lightDesktop, theme.bg.darkDesktop)}
-        />
-      </Box>
-      <Container
-        maxW={["90%", "80%", "70%", "65%", "50%"]}
-        variant="main"
-        top={["50px"]}
-        left="50%"
-      >
-        <Header />
-        <TodoInput />
-        <Todo />
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Box>
+				<Image
+					w="100%"
+					h="15rem"
+					fit="cover"
+					src={useColorModeValue(
+						theme.bg.lightDesktop,
+						theme.bg.darkDesktop
+					)}
+				/>
+			</Box>
+			<Container
+				maxW={["90%", "80%", "70%", "65%", "50%"]}
+				variant="main"
+				top={["50px"]}
+				left="50%"
+			>
+				<Header />
+				<TodoInput />
+				<Todo />
+			</Container>
+		</>
+	);
 }
 
 export default App;
